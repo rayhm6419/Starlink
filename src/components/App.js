88b4cb19a791c95b8cfd95ca9bf10f272029fe82
rayhm6419 <haoml6419@gmail.com>
2023-04-02 00:27:01 -0700

    css and js implementaion

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import '../styles/App.css';
+import logo from '../assets/images/spacex_logo.svg'
+import Header from './Header'
+import Footer from './Footer';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
